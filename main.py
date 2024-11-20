a674c21af49c61ec99ccfedd6fb804f9af0038e7
jta00007 <jta00007@mix.wvu.edu>
2024-11-20 15:21:56 -0500

    Updated to show literally nothing

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def main():
     pygame.init()
     display = (800, 600)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
 
     while True:
         for event in pygame.event.get():
