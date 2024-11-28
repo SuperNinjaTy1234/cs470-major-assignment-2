275942af608a62eb7988927f22c96b9d82ba972b
emilyaman1 <eea0009@mix.wvu.edu>
2024-11-28 00:50:32 -0500

    Major changes for camera, houses, doors
    If door is in view, press o to open (haven't adjusted position yet). Camera had to be redone due to issues with model matrix. Added streamlined way to import blender objects.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 #The main file of the program
 import pygame
+import numpy as np
 from pygame.locals import *
 from OpenGL.GLU import *
 from OpenGL.GL import *
@@ -9,65 +10,180 @@ from OpenGL.GL import *
 from ground_plane import *
 from body_of_water import *
 from houses import *
-from imported_house import draw_imported_house
-
 from roads import *
-from imported_grocery_store import *
-from imported_barn import *
-from light import setup_daylight, setup_nightlight, enable_nighttime_lighting, enable_daytime_lighting
-from background import load_texture, draw_background
+from light import setup_daylight, enable_nighttime_lighting, enable_daytime_lighting
+from background import load_texture
+from scene_object import SceneObject
 
+# If you are importing through blender, use the SceneObject like below and it will take care of everything.
+# It already will render because there is a for loop that loops through all scene objects
+imported_house1 = SceneObject("Models/House.obj", scaled_size=4,name="imported_house1")
+door1 = SceneObject('Models/door.obj', scaled_size=1.6, name="door1")
+imported_house2 = SceneObject("Models/House2.obj", scaled_size=4, name="imported_house2")
+door2 = SceneObject("Models/door2.obj", scaled_size=1.6, name="door2")
+grocery_store = SceneObject("Models/grocery_store.obj", scaled_size=5, name="grocery_store")
+barn = SceneObject("Models/barn.obj", scaled_size=5, name="barn")
+scene_objects = [
+    {"object": imported_house1, "position": (8, 2.05, -7), "rotation": (0, 270, 0)},
+    {"object": door1, "position": (8, 1, -5.3), "rotation": (0, 270, 0)},
+    {"object": imported_house2, "position": (-7, 2, -7), "rotation": (0, 270, 0)},
+    {"object": door2, "position": (-7, 1, -5.3), "rotation": (0, 270, 0)},
+    {"object": grocery_store, "position": (-35, 1.5, -5), "rotation": (0, 0, 0)},
+    {"object": barn, "position": (-17.5, 2.5, -10), "rotation": (0, 0, 0)},
+]
+
+# Had to implement major changes to camera in order for positions to work correctly
+# so doors could be opened and eventually a car can be moved
+class Camera:
+    def __init__(self):
+        self.position = np.array([0.0, 2.0, 10.0], dtype=np.float64)  # Initial position
+        self.target = np.array([0.0, 0.0, 0.0], dtype=np.float64)  # Look at origin
+        self.up = np.array([0.0, 1.0, 0.0], dtype=np.float64)  # Up direction
+
+        # Movement flags
+        self.move_x = 0
+        self.move_y = 0
+        self.move_z = 0
+        self.rotate_horizontal = 0
+        self.rotate_vertical = 0
+
+        self.speed = 8.0
+        self.rotation_speed = 60.0
+
+    def update_position(self, delta_time):
+        forward = self.target - self.position
+        forward[1] = 0
+        forward /= np.linalg.norm(forward)  # Normalize
+
+        dx = self.move_x * self.speed * delta_time
+        dy = self.move_y * self.speed * delta_time
+        dz = self.move_z * self.speed * delta_time
+
+        self.position += forward * dz
+        self.position[0] += dx
+        self.position[1] += dy
+
+        self.target += forward * dz
+        self.target[0] += dx
+        self.target[1] += dy
+
+
+    def rotate_camera(self, delta_time):
+        if self.rotate_horizontal or self.rotate_vertical:
+            if self.rotate_horizontal != 0:
+                angle = np.radians(self.rotate_horizontal * self.rotation_speed * delta_time)
+                rotation_matrix = np.array([
+                    [np.cos(angle), 0, np.sin(angle)],
+                    [0, 1, 0],
+                    [-np.sin(angle), 0, np.cos(angle)],
+                ])
+                direction = self.target - self.position
+                self.target = self.position + np.dot(rotation_matrix, direction)
+
+            if self.rotate_vertical != 0:
+                angle = np.radians(self.rotate_vertical * self.rotation_speed * delta_time)
+                rotation_matrix = np.array([
+                    [1, 0, 0],
+                    [0, np.cos(angle), -np.sin(angle)],
+                    [0, np.sin(angle), np.cos(angle)],
+                ])
+                direction = self.target - self.position
+                direction = np.dot(rotation_matrix, direction)
+
+                direction[1] = np.clip(direction[1], -10, 10)
+                self.target = self.position + direction
+
+
+def toggle_visible_doors(camera):
+    door_objects = [obj for obj in scene_objects if "door" in obj["object"].name.lower()]
+    closest_door = None
+    closest_distance = float("inf")
+
+    for obj in door_objects:
+        scene_obj = obj["object"]
+        door_position = np.array(obj["position"])
+
+        is_visible = is_door_visible(camera, door_position)
+        print(f"{scene_obj.name} at {door_position} is {'visible' if is_visible else 'not visible'}")
+
+        if is_visible:
+            distance = np.linalg.norm(door_position - camera.position)
+            if distance < closest_distance:
+                closest_door = scene_obj
+                closest_distance = distance
+
+    if closest_door:
+        closest_door.open = not closest_door.open
+        print(f"Toggled {closest_door.name}. New state: {'Open' if closest_door.open else 'Closed'}")
+
+
+def is_door_visible(camera, door_position):
+    to_door = door_position - camera.position
+    to_door /= np.linalg.norm(to_door)  # Normalize
+
+    forward = camera.target - camera.position
+    forward /= np.linalg.norm(forward)  # Normalize
+    if np.dot(forward, to_door) <= 0:
+        return False
+    fov_cosine = np.cos(np.radians(45))
+    if np.dot(forward, to_door) < fov_cosine:
+        return False
+
+    distance = np.linalg.norm(door_position - camera.position)
+    if distance > 15:
+        return False
+    return True
 
 def main():
     pygame.init()
     display = (800, 600)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-    glTranslatef(0.0, 0.0, -10) #Must push the camera because it spawns inside the cube
-    glRotatef(15, 1, 0, 0)
 
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)  # Match the minimal test
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
     bg_texture = load_texture("Textures/mountain.jpg")  # Load the mountain texture
-
+    camera = Camera()
     setup_daylight()  # Start with daylight
 
-    #Enabled features
     glEnable(GL_DEPTH_TEST)
     glDepthFunc(GL_LESS)
     glEnable(GL_COLOR_MATERIAL)
 
-    move_on_x = 0
-    move_on_y = 0
-    move_on_z = 0
-    rotate = 0
-    rotate_up_or_down = 0
-    is_daytime = True  # Lighting state
 
+    is_daytime = True  # Lighting state
+    clock = pygame.time.Clock()
     while True:
+        delta_time = clock.tick(60) / 1000.0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
-                    move_on_x = 1
+                    camera.move_x = -1
                 elif event.key == pygame.K_d:
-                    move_on_x = -1
+                    camera.move_x = 1
                 elif event.key == pygame.K_s:
-                    move_on_z = -1
+                    camera.move_z = -1
                 elif event.key == pygame.K_w:
-                    move_on_z = 1
+                    camera.move_z = 1
                 elif event.key == pygame.K_r:
-                    rotate = 1
-                elif event.key == pygame.K_l:
-                    rotate = -1
-                elif event.key == pygame.K_m: #rotates camera up (pans up)
-                    rotate_up_or_down = 1
-                elif event.key == pygame.K_n: #rotates camera down (pans down)
-                    rotate_up_or_down = -1
+                    camera.rotate_horizontal = -1
+                elif event.key == pygame.K_q:
+                    camera.rotate_horizontal = 1
+                elif event.key == pygame.K_m:
+                    camera.rotate_vertical = 1
+                elif event.key == pygame.K_n:
+                    camera.rotate_vertical = -1
                 elif event.key == pygame.K_UP:
-                    move_on_y = -1
+                    camera.move_y = 1
                 elif event.key == pygame.K_DOWN:
-                    move_on_y = 1
+                    camera.move_y = -1
+                elif event.key == pygame.K_o:
+                    toggle_visible_doors(camera)
                 elif event.key is pygame.K_t:  # Toggle lighting
                     is_daytime = not is_daytime
                     if is_daytime:
@@ -75,26 +191,27 @@ def main():
                     else:
                         enable_nighttime_lighting()
             if event.type == pygame.KEYUP:
-                if event.key == pygame.K_a:
-                    move_on_x = 0
-                elif event.key == pygame.K_d:
-                    move_on_x = 0
-                elif event.key == pygame.K_s:
-                    move_on_z = 0
-                elif event.key == pygame.K_w:
-                    move_on_z = 0
-                elif event.key == pygame.K_r:
-                    rotate = 0
-                elif event.key == pygame.K_l:
-                    rotate = 0
-                elif event.key == pygame.K_m:
-                    rotate_up_or_down = 0
-                elif event.key == pygame.K_n:
-                    rotate_up_or_down = 0
-                elif event.key == pygame.K_UP:
-                    move_on_y = 0
-                elif event.key == pygame.K_DOWN:
-                    move_on_y = 0
+                if event.key in (pygame.K_a, pygame.K_d):
+                    camera.move_x = 0
+                elif event.key in (pygame.K_w, pygame.K_s):
+                    camera.move_z = 0
+                elif event.key in (pygame.K_q, pygame.K_r):
+                    camera.rotate_horizontal = 0
+                elif event.key in (pygame.K_m, pygame.K_n):
+                    camera.rotate_vertical = 0
+                elif event.key in (pygame.K_UP, pygame.K_DOWN):
+                    camera.move_y = 0
+
+        camera.update_position(delta_time)
+        camera.rotate_camera(delta_time)
+
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        gluLookAt(
+            camera.position[0], camera.position[1], camera.position[2],
+            camera.target[0], camera.target[1], camera.target[2],
+            camera.up[0], camera.up[1], camera.up[2]
+        )
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
@@ -106,24 +223,10 @@ def main():
         house()
         house2()
         house3()
-        house4()
-        house5()
 
-        draw_imported_grocery_store()
-        draw_imported_barn()
-        draw_imported_house()
-
-
-        glTranslatef(move_on_x, move_on_y, move_on_z)
-
-        #For some reason, this makes the camera get farther and farther away
-        if rotate == 1 or rotate == -1:
-            glRotatef(10, 0, rotate, 0)
-
-        if rotate_up_or_down == 1 or rotate_up_or_down == -1:
-            glRotatef(10, rotate_up_or_down, 0, 0)
+        for obj in scene_objects:
+            obj["object"].render(position=obj["position"], rotation=obj["rotation"], open_rotation=90)
 
         pygame.display.flip()
-        pygame.time.wait(75)
 
 main()
---
+++ scene_object.py
@@ -0,0 +1,69 @@
+import numpy as np
+from OpenGL.GL import *
+import pywavefront
+
+
+class SceneObject:
+    def __init__(self, model_path, scaled_size, name=None):
+        """
+              Initialize a SceneObject with its model data and scaling.
+
+              Parameters:
+                  model_path (str): Path to the model file.
+                  scaled_size (float): Desired maximum size after scaling.
+        """
+
+        self.model = pywavefront.Wavefront(model_path, collect_faces=True)
+        self.name = name if name else model_path
+        bbox_min = np.min(self.model.vertices, axis=0)
+        bbox_max = np.max(self.model.vertices, axis=0)
+        self.bounding_box = (bbox_min, bbox_max)
+
+        self.size = bbox_max - bbox_min
+        self.max_size = np.max(self.size)
+        self.scale = [scaled_size / self.max_size for _ in range(3)]
+        self.translation = -((bbox_max + bbox_min) / 2)
+
+        self.open = False
+
+    def render(self, position, rotation=(0, 0, 0), open_rotation=None):
+        """
+        Render the object at a given position with optional rotation.
+
+        Parameters:
+            position (tuple): World position (x, y, z).
+            rotation (tuple): Rotation angles (x, y, z) in degrees.
+            open_rotation (float): Additional rotation if the object is "open."
+        """
+        glPushMatrix()
+        glTranslate(*position)
+        glRotatef(rotation[0], 1, 0, 0)
+        glRotatef(rotation[1], 0, 1, 0)
+        glRotatef(rotation[2], 0, 0, 1)
+
+        if self.open and open_rotation:
+            glRotatef(open_rotation, 0, 1, 0)
+
+        self._render_object()
+        glPopMatrix()
+
+    def _render_object(self):
+        """
+        Internal method to render the object using its scale and translation.
+        """
+        glPushMatrix()
+        glScalef(*self.scale)
+        glTranslatef(*self.translation)
+
+        for mesh in self.model.mesh_list:
+            material_name = mesh.materials[0].name
+            material = self.model.materials.get(material_name)
+
+            glColor(material.diffuse)
+            glBegin(GL_TRIANGLES)
+            for face in mesh.faces:
+                for vertex_i in face:
+                    glVertex3f(*self.model.vertices[vertex_i])
+            glEnd()
+
+        glPopMatrix()
